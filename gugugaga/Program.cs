ac9c21d01f2cadff1c19fa9b6c3d6fc65943c27c
F0RZENT <113331099+F0RZENT@users.noreply.github.com>
2023-09-25 03:04:54 +0500

    Add files via upload

---
+++ gugugaga/Program.cs
@@ -0,0 +1,15 @@
+﻿public class multiplication{
+    static void Main(){
+
+    int a = 5;
+
+    for (int i = 1; i < 10; i++){
+
+        int result = a * i;        
+        Console.WriteLine(result);
+        }    
+    
+    }
+    
+
+}
